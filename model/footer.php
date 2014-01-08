f133650a3d2e8121105c866b41026ef875954d3b
Josh Wang <spjay1@gmail.com>
2014-01-08 21:43:50 +0800

    add js lib

--- model/footer.php
+++ model/footer.php
@@ -14,6 +14,8 @@
 	<![endif]-->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.imagemapster.min.js"></script>
+		<script src="js/script.js"></script>
 	
 </body>
 </html>
